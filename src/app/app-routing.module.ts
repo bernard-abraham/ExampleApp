35de856c1e37ab8be3e9150e554352da93145fe7
bernard-abraham <b.abrhm@gmail.com>
2023-02-18 01:49:23 +0000

    added router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InfoboxComponent } from './components/infobox/infobox.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'first-component', component: InfoboxComponent },
+{ path: 'second-component', component: SearchBarComponent },
+{ path: '', component: SidebarComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
