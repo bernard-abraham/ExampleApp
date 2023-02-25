c12db7ea381d49f0cc00100c7f012d6efba2186c
Bernard Abraham <bernardabraham@Bernards-MacBook-Pro.local>
2023-02-25 16:36:07 +0000

    added grid layout

--- src/app/components/infobox/infobox.component.ts
+++ src/app/components/infobox/infobox.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class InfoboxComponent implements OnInit {
   numbers = [1,2];
   constructor() { 
-    this.numbers = [1,2,3,4,5,6];
+    this.numbers = [1,2,3,4,5,6,7,8,9,10];
   }
 
   ngOnInit(): void {
